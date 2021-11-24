3aad725dcf91914ba872f80020fce0ecd319593b
Mark Momot <markmomot03@gmail.com>
2021-11-24 10:04:57 -0500

    commit tree 97c55a81afdd15abdefa997715f78c3a4fc89363

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,17 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  console.log(actors)
+  const actorElements= actors.map(actor => <div key={actor.name}>
+    <h4>Name: {actor.name}</h4>
+    <p>Movies: </p>
+    <ul>{actor.movies.map(movie => <li key={movie}>{movie}</li>)}</ul>
+  </div>)
+
+  return <div>
+      <h1>Actors Page</h1>
+      {actorElements}
+    </div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,16 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorElements= directors.map(director => <div key={director.name}>
+    <h4>Name: {director.name}</h4>
+    <p>Movies: </p>
+    <ul>{director.movies.map(movie => <li key={movie}>{movie}</li>)}</ul>
+  </div>)
+
+  return <div>
+      <h1>Directors Page</h1>
+      {directorElements}
+    </div>;
 }
 
 export default Directors;
